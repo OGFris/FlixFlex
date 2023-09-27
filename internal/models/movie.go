f8476f58ad13892aa134c01756c8ebd1ce69b23d
Ilyes <ilyes.cherfaoui@protonmail.com>
2023-09-27 19:27:29 +0100

    Fixed .env.example and modified movies table conditions

--- internal/models/movie.go
+++ internal/models/movie.go
@@ -2,8 +2,8 @@ package models
 
 type Movie struct {
 	Model
-	Title     string `gorm:"not null"`
+	Title     string `gorm:"not null;unique"`
 	Synopsis  string `gorm:"not null"`
 	PosterUri string `gorm:"not null"`
-	TmdbID    uint   `gorm:"not null"`
+	TmdbID    uint   `gorm:"not null;unique"`
 }
